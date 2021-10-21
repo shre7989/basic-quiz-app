0e22b22fe5f6018a18f0cf48c54604e5f594dc8c
shre7989 <Shre7989@unm.edu>
2021-10-21 00:54:44 -0600

    learning flutter fundamentals

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,42 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ['What is the tallest mountain on Earth?'];
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
             title: Text('My First App Bar'), backgroundColor: Colors.red[700]),
-        body: Text('Default text'),
+        body: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 20,
+            ),
+            Align(
+              alignment: Alignment.center,
+            ),
+            Text(questions[0]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+          crossAxisAlignment: CrossAxisAlignment.center,
+        ),
       ),
     );
     //throw UnimplementedError();
